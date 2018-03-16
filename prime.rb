1d41e492a281172415d830ff2eeb0f6085647144
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-16 22:36:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(int)
   if int < 0 || int == 0 || int == 1
     return false
   else
-    (2..int-1).to_a.any? do |some_factor|
+    (2..int-1).to_a.all? do |some_factor|
       int % some_factor != 0
     end
   end
