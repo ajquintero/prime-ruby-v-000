03c2b8dfaa4abe0517b0cde627616f63b7586450
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-16 22:18:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 # Add  code here!
 def prime?(int)
-  if int < 0 || int == 0 || int = 1
-    return false
-  elsif 
-
-
+  (2...int).each do |has_divisor|
+    if int % has_divisor == 0
+      return false
+    end
+    true
+  end
+  # if int < 0 || int == 0 || int = 1
+  #   return false
+  #
+  # elsif
 end
